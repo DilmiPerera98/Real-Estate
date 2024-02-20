e59541b92a5bf1f2c43ff3b64da9374494b92064
Dilmi <dkaushani98@gmail.com>
2024-02-20 14:40:25 +0530

    create signUp api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+//if there is await in the function, it should be async
+export const signup = async (req, res) => {
+  // Destructure the request body
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 12);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  // Save the user to the database
+  //this take few times to save
+  //so that we use async and await to prevent error
+  //then it will wait until the user is saved and go to the next line
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
