684c05c3f660da820991d9eb933754aa4cb6dc87
Dilmi <dkaushani98@gmail.com>
2024-03-22 22:53:08 +0530

    Complete delete listing

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { creatListing } from "../controllers/listing.controller.js";
+import {
+  creatListing,
+  deleteListing,
+} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/VerifyUser.js";
 
 const router = express.Router();
 router.post("/create", verifyToken, creatListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
 export default router;
