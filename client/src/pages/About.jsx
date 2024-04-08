d7255cd038c58a2aef9404e4f402e4353d9f5aa7
Dilmi <dkaushani98@gmail.com>
2024-04-08 18:47:36 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,33 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className=" mt-2 py-200 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Find Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        This word has two main meanings. The first has to do with being pleased
+        and satisfied (feeling content) or making someone else feel happy and at
+        peace with things (contenting them). The other meaning has to do with
+        subject matter: the content of a history class might be American
+        history. The content of a math class might be geometry. As long as
+        there's a topic or subject, there's content.
+      </p>
+      <p className="mb-4 text-slate-700">
+        This word has two main meanings. The first has to do with being pleased
+        and satisfied (feeling content) or making someone else feel happy and at
+        peace with things (contenting them). The other meaning has to do with
+        subject matter: the content of a history class might be American
+        history. The content of a math class might be geometry. As long as
+        there's a topic or subject, there's content.
+      </p>
+      <p className="mb-4 text-slate-700">
+        This word has two main meanings. The first has to do with being pleased
+        and satisfied (feeling content) or making someone else feel happy and at
+        peace with things (contenting them). The other meaning has to do with
+        subject matter: the content of a history class might be American
+        history. The content of a math class might be geometry. As long as
+        there's a topic or subject, there's content.
+      </p>
+    </div>
+  );
 }
